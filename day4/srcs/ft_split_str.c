8317d96d5143483a4bd8d6a03b3737c98b933a98
MVPee <mariusvanpee@hotmail.be>
2023-12-04 20:24:32 +0100

    day4

---
+++ day4/srcs/ft_split_str.c
@@ -0,0 +1,102 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split_str.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mvpee <mvpee@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/02 17:40:24 by mvpee             #+#    #+#             */
+/*   Updated: 2023/12/04 19:01:53 by mvpee            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/day4.h"
+
+int	separator_check(char a, char *charset)
+{
+	int	i;
+
+	i = 0;
+	while (charset[i])
+	{
+		if (charset[i] == a)
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+int	total_string(char *str, char *charset)
+{
+	int	i;
+	int	count;
+
+	count = 0;
+	i = 0;
+	while (str[i])
+	{
+		while (str[i] && separator_check(str[i], charset))
+			i++;
+		if (str[i])
+			count++;
+		while (str[i] && !separator_check(str[i], charset))
+			i++;
+	}
+	return (count);
+}
+
+int	sep_len(char *str, char *charset)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] && !separator_check(str[i], charset))
+		i++;
+	return (i);
+}
+
+char	*ft_word(char *str, char *charset)
+{
+	int		len_word;
+	int		i;
+	char	*word;
+
+	i = 0;
+	len_word = sep_len(str, charset);
+	word = (char *)malloc(sizeof(char) * (len_word + 1));
+	while (i < len_word)
+	{
+		word[i] = str[i];
+		i++;
+	}
+	word[i] = '\0';
+	return (word);
+}
+
+char	**ft_split_str(char *str, char *charset)
+{
+	char	**strings;
+	int		i;
+
+	i = 0;
+	if (!str || !charset)
+		return (0);
+	strings = (char **)malloc(sizeof(char *)
+			* (total_string(str, charset) + 1));
+	if (!strings)
+		return (0);
+	while (*str)
+	{
+		while (*str && separator_check(*str, charset))
+			str++;
+		if (*str != '\0')
+		{
+			strings[i] = ft_word(str, charset);
+			i++;
+		}
+		while (*str && !separator_check(*str, charset))
+			str++;
+	}
+	strings[i] = NULL;
+	return (strings);
+}
---
+++ day4/srcs/main.c
@@ -0,0 +1,117 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mvpee <mvpee@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/04 18:53:03 by mvpee             #+#    #+#             */
+/*   Updated: 2023/12/04 20:23:55 by mvpee            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/day4.h"
+
+static int count_win(char **split)
+{
+    int count;
+    int i;
+
+    count = 0;
+    i = 1;
+    while(split[i] && ft_strcmp(split[i], "|") != 0)
+    {
+        count++;
+        i++;
+    }
+    return count;
+}
+
+static int count_num(char **split)
+{
+    int count;
+    int i;
+
+    count = 0;
+    i = 0;
+    while(split[i] && ft_strcmp(split[i], "|") != 0)
+        i++;
+    i++;
+    while(split[i])
+    {
+        i++;
+        count++;
+    }
+    return count;
+}
+
+static int process(int *win_array, int *num_array)
+{
+    int total;
+    int i, j;
+
+    i = 0;
+    j = 0;
+    total = 0;
+    while(win_array[i])
+    {
+        j = 0;
+        while(num_array[j])
+        {
+            if(win_array[i] == num_array[j])
+                if(total == 0)
+                    total = 1;
+                else
+                    total <<= 1;
+            j++;
+        }
+        i++;
+    }
+    return total;
+}
+
+static int parsing(char **split)
+{
+    int *win;
+    int *num;
+    int i, j;
+    int total;
+
+    win = (int *)malloc(sizeof(int) * (count_win(split) + 1));
+    num = (int *)malloc(sizeof(int) * (count_num(split) + 1));
+
+    i = 1;
+    j = 0;
+    while(split[i] && ft_strcmp(split[i], "|"))
+        win[j++] = ft_atoi(split[i++]);
+    win[j] = NULL;
+    i++;
+    j = 0;
+    while(split[i])
+        num[j++] = ft_atoi(split[i++]);
+    
+    total = process(win, num);
+    free(win);
+    free(num);
+    return total;
+}
+
+int main(void)
+{
+    char *line;
+    char **split;
+    int i;
+    int total = 0;
+    int fd = open("rscs/input.txt", O_RDONLY);
+    while(line = get_next_line(fd))
+    {
+        split = ft_split_str(line, " Card");
+        free(line);
+        i = 0;
+        total += parsing(split);
+        ft_free_split(split);
+    }
+    ft_printf("total %d\n", total);
+    close(fd);
+    return 0;
+}
